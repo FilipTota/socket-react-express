b1ce57aa9d7baea1ba4381e0329c555d7bc74a5b
filiptota <filip.tota5@gmail.com>
2025-02-13 14:03:01 +0100

    implementing socket.io to both frontend and backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
+import { useEffect } from "react";
 import "./App.css";
+import io from "socket.io-client";
+
+const socket = io.connect(`${import.meta.env.VITE_BACKEND_URL}`);
 
 function App() {
-  return <>Hello</>;
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      alert(data.message);
+    });
+  }, [socket]);
+
+  const sendMessage = () => {
+    socket.emit("send_message", { message: "Hello" }); // "send_message" -> event name, second parametes is the data we want to send to socket on backend
+  };
+  return (
+    <div className="app">
+      <input type="text" placeholder="Message..." />
+      <button onClick={sendMessage}>Send</button>
+    </div>
+  );
 }
 
 export default App;
---
+++ server/server.js
@@ -0,0 +1,41 @@
+import dotenv from "dotenv";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
+
+dotenv.config();
+const port = process.env.PORT || 8081;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// http server -> socket.io needs an HTTP server to attach to
+const server = http.createServer(app);
+
+// io will allow us to work with socket.io
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", // link for our frontend
+  },
+});
+
+// listen to event
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  // connection is established and we can access the same connection with different browsers from different users that are on the same URL ("http://localhost:5173")
+
+  // on the backend we listen for the "send_message" event that we get from frontend
+  // data is the data sent from frontend
+  socket.on("send_message", (data) => {
+    // send data to everyone who is connected to the socket server:
+    // broadcast -> allows us to send something to everyone but self
+    socket.broadcast.emit("receive_message", data);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
